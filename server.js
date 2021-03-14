b69659feb3c84006e30ea2bd56237b649b5a5d9d
Andy 8421さん <48783339+yichang8421@users.noreply.github.com>
2021-03-14 21:04:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -19,24 +19,46 @@ var server = http.createServer(function(request, response){
 
   /******** 从这里开始看，上面不要看 ************/
 
-  console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+  console.log('有人发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+    console.log('method: ' + method) //GET或POST
+    console.log('request.headers:')
+    console.log(request.headers)
 
-  if(path === '/'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你好！`)
-    response.end()
-  } else if(path === '/x'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/css;charset=utf-8')
-    response.write(`body{color: red;}`)
-    response.end()
-  } else {
-    response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你输入的路径不存在对应的内容`)
-    response.end()
-  }
+    if(path === '/'){
+        // console.log("有人访问/了")
+        response.statusCode = 200           //设置状态码
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')       //设置响应头，编码格式
+        var accept = request.headers["accept"]
+        console.log("accept:")
+        console.log(accept)
+        response.write(`                
+            <!DOCTYPE html>
+            <head>
+                <link rel="stylesheet" href="/x">
+            </head>
+            <body>
+                <h1>你好</h1>
+                <script src="/y"></script>
+            </body>
+        `)                      //设置响应体
+        response.end()          //结束响应
+    } else if(path === '/x'){
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/css;charset=utf-8')
+        response.write(`body{color: red;}\n`)
+        response.end()
+    } else if (path === '/y'){
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+        response.write(`console.log('这是js的内容')`)
+        response.end()
+    }
+    else {
+        response.statusCode = 404
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.write(`你输入的路径不存在对应的内容\n`)
+        response.end()
+    }
 
   /******** 代码结束，下面不要看 ************/
 })
